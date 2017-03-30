38fb9e70fb796fb5478d5c0d6812560abf1371e2
Aman <bzcheeseman@gmail.com>
2017-03-30 13:19:35 -0500

    Working on getting things working, don't totally understand embedding/de-embedding in CGRUd

--- NGPU/neural_gpu.py
+++ NGPU/neural_gpu.py
@@ -15,7 +15,7 @@ from Utils import CGRUCell, CGRUdCell
 
 
 class NeuralGPU(nn.Module):
-    def __init__(self, I, w, h, l_cgru, l_cgrud, m):
+    def __init__(self, I, l_cgru, l_cgrud, m):
         super(NeuralGPU, self).__init__()
 
         self.E = nn.Embedding(I, m)
@@ -33,29 +33,8 @@ class NeuralGPU(nn.Module):
 
         o_k = self.O.mm(d_tp1[0, k])
         p_tp1 = p_t
-        p_tp1[0, k] = self.E.mm(o_k)
-
-
-
-class Encode(nn.Module):  # need to embed the input into the initial state
-    def __init__(self, i, w, n, m=24):
-        super(Encode, self).__init__()
-
-        self.I = i
-        self.w = w
-        self.n = n
-        self.m = m
-        self.embed = nn.Parameter(torch.rand(self.I, m))
-
-    def forward(self, x):
-        # input sequence i is a sequence of n discrete symbols from {0...I} - see code example if possible
-        batch = x.size()[0]
-        s0 = Variable(torch.zeros(batch, self.m, self.n, self.w))
-        # embed x into s0
-        return s0
-
+        p_tp1[0, k] = self.Ep.mm(o_k.long())
 
 if __name__ == "__main__":
     cgru = CGRUCell()
-    encoder = Encode(1, 3, 6)
 
